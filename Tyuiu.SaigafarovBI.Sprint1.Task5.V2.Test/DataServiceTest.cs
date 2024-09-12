d0c90d8565f2c19d8113f79f5e052f9bcb55cb4f
Tyuiu.SaigafarovBI@outlook.com <sajgafarovb@gmail.com>
2024-09-12 18:46:47 +0500

    Task5

---
+++ Tyuiu.SaigafarovBI.Sprint1.Task5.V2.Test/DataServiceTest.cs
@@ -0,0 +1,14 @@
+using Tyuiu.SaigafarovBI.Sprint1.Task5.V2.Lib;
+namespace Tyuiu.SaigafarovBI.Sprint1.Task5.V2.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            Assert.AreEqual(27, ds.FahrenheitTo—elsius(300.0));
+        }
+    }
+}
---
+++ Tyuiu.SaigafarovBI.Sprint1.Task5.V2/Program.cs
@@ -0,0 +1,22 @@
+﻿using Tyuiu.SaigafarovBI.Sprint1.Task5.V2.Lib;
+namespace Tyuiu.SaigafarovBI.Sprint1.Task5.V2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("Введите X:");
+            double x = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine(ds.FahrenheitToСelsius(x));
+            Console.ReadKey();
+        }
+    }
+}
